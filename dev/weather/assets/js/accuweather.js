4bcbfb57942c5fde04040d45f1ab42f56231a11b
Achintya De <achintya.de@thequint.com>
2017-10-24 18:38:32 +0530

    update

--- dev/weather/assets/js/accuweather.js
+++ dev/weather/assets/js/accuweather.js
@@ -3,8 +3,7 @@
 
 var city_id,city_name,city_temp,city_cond, forecasts_data;
 
-var city_array=["Leh","Srinagar"];
-var city_temp_array =[]
+
 
     function get_weather(id) { // for current location
 	    $.ajax({
@@ -12,7 +11,7 @@ var city_temp_array =[]
             dataType: "jsonp",
             jsonp: "callback",
             jsonpCallback: "callback",
-            url: "http://apidev.accuweather.com/currentconditions/v1/"+id+".json?language=en&apikey=hoArfRosT1215",
+            url: "http://apidev.accuweather.com/currentconditions/v1/"+id+".json?language=en&apikey=1bbb649ae0174399b320067bbc1c3aa8",
             cache: false,
 
             success: function (data) {
@@ -20,6 +19,7 @@ var city_temp_array =[]
 				city_cond= data[0].WeatherText;
            		console.log(city_temp,city_cond);
 				append_data();
+				getForecastsInfo(city_id);
            		
 				
             }
@@ -34,33 +34,62 @@ var city_temp_array =[]
 	        dataType: "jsonp",
 	        jsonp: "callback",
 	        jsonpCallback: "callback",
-	        url: "http://apidev.accuweather.com/locations/v1/cities/geoposition/search.json?q="+x+","+y+"&apikey=hoArfRosT1215",
+	        url: "http://apidev.accuweather.com/locations/v1/cities/geoposition/search.json?q="+x+","+y+"&apikey=1bbb649ae0174399b320067bbc1c3aa8",
 	        cache: false,
 
 	        success: function (data) {
+				
 				city_name = data.LocalizedName;
 				city_id = data.Key;
 				
 				get_weather(city_id);
-				//getForecastsInfo(city_id);
+				console.log(data);
 				console.log(city_name+"|"+city_id);
+				
 				
 				
 	        }
 	  	});
     }
 
-	function getForecastsInfo(city_id){ // for current location
+Date.prototype.addDays = function(days) {
+    this.setDate(this.getDate() + parseInt(days));
+    return this;
+};
+
+	function getForecastsInfo(city_id){ 
+		console.log(city_id);
     	$.ajax({
 	        type: "GET",
 	        dataType: "jsonp",
 	        jsonp: "callback",
 	        jsonpCallback: "callback",
-	        url: "http://apidev.accuweather.com/forecasts/v1/daily/5day/"+city_id+"?apikey=hoArfRosT1215",
+	        url: "http://apidev.accuweather.com/forecasts/v1/daily/5day/"+city_id+"?apikey=1bbb649ae0174399b320067bbc1c3aa8&details=true",
 	        cache: false,
 
-	        success: function (data) {
-				console.log(data);
+	        success: function (forecast_data) {
+				//console.log(forecast_data);
+				//console.log(forecast_data.DailyForecasts[0].Temperature.Maximum.Value);
+				$('.local-weather--left').html("<img src='assets/images/icons/"+forecast_data.DailyForecasts[0].Day.Icon+".png'>");
+				for (var i=0; i<5; i++)
+							{
+								//console.log(d);
+								
+								var cur_date = new Date();
+								//console.log(cur_date.addDays(i))
+								
+								$(".daily-weather--slider").append("<li><h4 class='daily-weather--headline'>"+weekday[cur_date.addDays(i).getDay()]+"</h4><span class='daily-weather--icon'><img src='assets/images/icons/"+forecast_data.DailyForecasts[i].Day.Icon+".png'></span><div class='daily-weather--temp'><span class='small-temp'>"+forecast_data.DailyForecasts[i].Temperature.Maximum.Value+  '°F'+"</span><span class='large-temp'>"+forecast_data.DailyForecasts[i].Temperature.Minimum.Value+ '°F'+"</span></div></li>");
+								
+							}
+				
+				
+				
+				
+				
+				
+				
+				
+				//console.log(data.DailyForecasts[1].Temperature.Maximum.Value);
 				//forecasts_data = data[0].Headline.Text;
 				//console.log(forecasts_data);
 	        }
@@ -90,6 +119,10 @@ var city_temp_array =[]
 		$("#currentLocation").html(city_name);
 		$("#currentTemp").html(city_temp);
 		$("#currentCond").html(city_cond);
+		$('body').addClass(city_cond);
+		//$('.local-weather--left').html("<img src='assets/images/icons/"+forecast_data.DailyForecasts[0].Day.Icon+".png'>");
+		
+		
 	}
 
 	
@@ -99,26 +132,54 @@ var city_temp_array =[]
 
 	// begin listing 
 
+	//var city_array=["Agra","Srinagar","Delhi","kolkata","Bangalore"];
+	var city_array=["Agra","Mumbai","Chennai","Kolkata","Bangalore","Lucknow","Assam","Rajasthan"];
+
+	var city_id_array =[];
+	var city_weather_array = []; // use this
+	var city_list_array_count = 0;
+	var id_list_array_count = 0;
 
 
-	function get_weather_array(id) { // for the city listing
-		console.log("ID"+id);
+	function get_weather_list(id) { // for current location
 	    $.ajax({
             type: "GET",
             dataType: "jsonp",
             jsonp: "callback",
             jsonpCallback: "callback",
-            url: "http://apidev.accuweather.com/currentconditions/v1/"+id+".json?language=en&apikey=hoArfRosT1215",
+            url: "http://apidev.accuweather.com/currentconditions/v1/"+id+".json?language=en&apikey=1bbb649ae0174399b320067bbc1c3aa8",
             cache: false,
 
             success: function (data) {
+				//city_temp = data[0].Temperature.Metric.Value+ "°C";
+				//city_cond= data[0].WeatherText;
+				city_weather_array.push(data[0].Temperature.Metric.Value+ "°C");
+				//console.log(city_weather_array);
+				id_list_array_count = id_list_array_count+1;
+           		//console.log("yes");
+				if(id_list_array_count<city_array.length)
+					{
+				append_city_weather_array(id_list_array_count)
+					}
+				else
+					{
+						console.log(city_weather_array);
+						// append list
+						for (var i=0;i<city_array.length;i++)
+							{
+								$(".city_list").append("<li><span class='city_name'>"+city_array[i]+"</span><span class='city_border'></span><span class='city_temp'>"+city_weather_array[i]+"</span></li>");
+								if (i==city_array.length-1){
+								reset_dash();
+								}
+							}
+					}
+			
 				
-           		city_temp_array.push(data[0].Temperature.Metric.Value);
-				console.log(city_temp_array);
             }
       	});	
     }
 
+	
    function getIdByCityName(city_name){ // For the city listing
 	  // alert(city_name)
 	    $.ajax({
@@ -126,21 +187,44 @@ var city_temp_array =[]
 	        dataType: "jsonp",
 	        jsonp: "callback",
 	        jsonpCallback: "callback",
-	        url: "http://apidev.accuweather.com/locations/v1/search?q="+city_name+",%20india&apikey=hoArfRosT1215",
+	        url: "http://apidev.accuweather.com/locations/v1/search?q="+city_name+",%20india&apikey=1bbb649ae0174399b320067bbc1c3aa8",
 	        cache: false,
 
 	        success: function (data) {
-	      // 		get_weather_array(data[0].Key)
+	            //get_weather_array(data[0].Key)
 	       		//console.log(data[0].Key)
+				//console.log(data[0].Key);
+				city_id_array.push(data[0].Key);
+				//console.log(city_id_array);
+				city_list_array_count = city_list_array_count+1;
+				if(city_list_array_count<city_array.length)
+					{
+				append_city_id_array(city_list_array_count)
+					}
+				else
+					{
+					append_city_weather_array(id_list_array_count);	
+					}
 	        }
 	  	});
 	}
 
-for(var i=0;i<city_array.length;i++)
-	{
-		console.log(city_array[i]);
-getIdByCityName(city_array[i]);
-	}
+
+function append_city_id_array(x)
+{
+	getIdByCityName(city_array[x]);
+	
+}
+
+function append_city_weather_array(x)
+{
+	get_weather_list(city_id_array[x]);
+	
+}
+append_city_id_array(city_list_array_count)
+
+
+
 
 
 
@@ -150,6 +234,14 @@ var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday
 
 document.getElementById("toDay").innerHTML = weekday[d.getDay()];
 
+var current_day_index = weekday.indexOf(weekday[d.getDay()]);
+
+
+	
+
+//alert(day_index);
+
+//alert(weekday[d.getDay()]+1);
 
 
 
--- dev/weather/assets/js/functions.js
+++ dev/weather/assets/js/functions.js
@@ -101,3 +101,69 @@ $(document).ready(function() {
 $(window).resize(function(){
     reset_dash();
 });
+
+
+
+
+// Story collections
+
+//Slug 1 
+
+$(document).ready(function() {
+  var collectionSlug = 'bol-ugc'; //Needs to be replaced.
+  $.getJSON('https://www.thequint.com/api/v1/collections/' + collectionSlug, function(res) {
+    var stories = res.items.filter(function(item) {
+      return item.type == 'story'
+    }).map(function(item) {
+      return item.story
+    }).slice(0,3);
+    var elements = stories.map(function(story) {
+      return '<li><a href="https://www.thequint.com/' + story.slug + '" target="blank">'+ story.headline +'</a></li>'
+    });
+    elements.forEach(function(element) {
+      $('#SlugType_1').append(element);
+    });
+  });
+});
+
+//Slug 2 
+
+$(document).ready(function() {
+  var collectionSlug = 'bol-feature'; //Needs to be replaced.
+  $.getJSON('https://www.thequint.com/api/v1/collections/' + collectionSlug, function(res) {
+    var stories = res.items.filter(function(item) {
+      return item.type == 'story'
+    }).map(function(item) {
+      return item.story
+    }).slice(0,6);
+    var elements = stories.map(function(story) {
+      return '<li><a href="https://www.thequint.com/' + story.slug + '" target="blank">'+ story.headline +'</a></li>'
+    });
+    elements.forEach(function(element) {
+      $('#SlugType_2').append(element);
+    });
+  });
+});
+
+// Slug 3
+
+$(document).ready(function() {
+  var collectionSlug = 'bol-video'; //Needs to be replaced.
+  $.getJSON('https://www.thequint.com/api/v1/collections/' + collectionSlug, function(res) {
+    var stories = res.items.filter(function(item) {
+      return item.type == 'story'
+    }).map(function(item) {
+      return item.story
+    }).slice(0,6);
+    var elements = stories.map(function(story) {
+      return '<li><figure><a href="https://www.thequint.com/' + story.slug + '" target="blank"><div><img src="https://images.assettype.com/' + story['hero-image-s3-key'] + '?q=70&w=672&fm=pjpg"></div><figcaption>'+ story.headline +'</figcaption></a></figure></li>'
+    });
+    elements.forEach(function(element) {
+      $('#SlugType_3').append(element);
+    });
+  });
+});
+
+
+
+
