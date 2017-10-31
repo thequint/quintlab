b901813a9b5805c228c7b1a022a94b534e784dd2
Achintya De <achintya.de@thequint.com>
2017-10-31 18:50:13 +0530

    update

--- dev/gujarat/assets/js/functions.js
+++ dev/gujarat/assets/js/functions.js
@@ -1,26 +1,77 @@
+$(document).ready(function () {
+	// share
+	$(".share-ico").click(function (e) {
+		$(".social-ico").slideToggle(400);
+		return false;
+	});
+	$("body").click(function () {
+		$(".social-ico").slideUp();
+	});
 
-$(document).ready(function(){
-    $(".share-ico").click(function(e){
-      $(".social-ico").slideToggle(400);
-      return false;
-   });
-   $("body").click(function(){
-      $(".social-ico").slideUp();
-   });
+	// move down
+
+	$(".move-down").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#grid").offset().top
+		}, 1000);
+
+
+
+
+	});
+
+	// car animation
 	
+	var lastScrollTop = 0;
+
+	$(window).scroll(function () {
+
+
+		clearTimeout($.data(this, 'scrollTimer'));
+		$.data(this, 'scrollTimer', setTimeout(function () {
+
+
+			var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+			if (st > lastScrollTop) {
+
+				console.log("down");
+
+				$(".car").removeClass("rotate");
+				// downscroll code
+			} else {
+				console.log("up");
+				$(".car").addClass("rotate");
+			}
+			lastScrollTop = st;
+
+
+
+			var car_move = $(window).scrollTop() - $(".header-wrapper").height() / 2;
+
+			if (car_move > -20) {
+				$(".car").css({
+					"top": $(window).scrollTop() - window.innerHeight / 2
+				})
+			} else {
+				$(".car").css({
+					"top": -20
+				})
+			}
+
+
+			console.log("Haven't scrolled in 250ms!");
+		}, 250));
+
+
+	})
 	
-	$(".move-down").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#grid").offset().top
-    }, 1000);
+	// end car animation
+
 });
-	
-	
+
+
+new AnimOnScroll(document.getElementById('grid'), {
+	minDuration: 0.4,
+	maxDuration: 0.7,
+	viewportFactor: 0.2
 });
-
-
-new AnimOnScroll( document.getElementById( 'grid' ), {
-	minDuration : 0.4,
-	maxDuration : 0.7,
-	viewportFactor : 0.2
-} );
