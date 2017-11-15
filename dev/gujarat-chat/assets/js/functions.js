31f4b496425c3f4167b79d8b245a85736d0a5c7a
Achintya De <achintya.de@thequint.com>
2017-11-15 15:45:52 +0530

    update ad

--- dev/gujarat-chat/assets/js/functions.js
+++ dev/gujarat-chat/assets/js/functions.js
@@ -18,26 +18,37 @@ $(document).ready(function () {
 
 	});
 	
+});
+
+
+//$('.show-el').click(function(e) {
+//  	e.preventDefault();
+//    var $this = $(this);
+//    if ($this.next().hasClass('hide-el')) {
+//        $this.next().removeClass('hide-el');
+//        $this.next().slideUp(350);
+//		$this.addClass('is-more');
+//    } else {
+//        $this.parent().parent().find('li .inner').removeClass('show');
+//        $this.parent().parent().find('li .inner').slideUp(350);
+//        $this.next().toggleClass('hide-el');
+//        $this.next().slideToggle(350);
+//    }
+//});
+
+
+$(".chat-container ul li").click(function(e){
 	
+	$(".chat-container ul li").removeClass("is-more").find(".hide-el").slideUp(350);
+	
+	$(this).addClass("is-more").find(".hide-el").slideDown(350);
+	
+//	if ($(this).hasClass("is-more")){
+//      $(this).removeClass("is-more").find(".hide-el").slideUp(350);
+//    }
 });
 
 
-$('.show-el').click(function(e) {
-  	/*e.preventDefault();
-  
-    var $this = $(this);
-  
-    if ($this.next().hasClass('hide-el')) {
-        $this.next().removeClass('hide-el');
-        $this.next().slideUp(350);
-		$this.addClass('is-more');
-    } else {
-        //$this.parent().parent().find('li .inner').removeClass('show');
-        //$this.parent().parent().find('li .inner').slideUp(350);
-        $this.next().toggleClass('hide-el');
-        $this.next().slideToggle(350);
-    }*/
-});
 
 
 
