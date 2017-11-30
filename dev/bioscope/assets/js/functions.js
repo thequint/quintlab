8cc4a83ac83629104d4b1d3a2d56bf1471302b26
Achintya De <achintya.de@thequint.com>
2017-11-30 18:32:31 +0530

    update

---
+++ dev/bioscope/assets/js/functions.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+	
+	// BioSlider
+	
+	
+//$('#BioSlider').slick({
+//  centerMode: true,
+//variableWidth: true,
+//  centerPadding: '0px',
+//  slidesToShow: 3,
+//  arrows: true
+//});
+	
+	
+	var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      //centeredSlides: true,
+      slidesPerView: 'auto',
+		 slidesPerView: 3,
+      coverflowEffect: {
+        rotate: 50,
+		 
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+//      pagination: {
+//        el: '.swiper-pagination',
+//      },
+		navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+	
+	
+
+	
+});
+
+
+
+
+ function moreData(){
+
+$(".chat-el").click(function(){
+	
+	if($(this).parent().hasClass("is-more")){
+		$(this).parent().removeClass("is-more").find(".hide-el").slideUp(350);
+		$(this).find(".frame-video").html("");
+	}
+	
+	else {
+		
+	var this_var = $(this);	
+		
+	$(".chat-el").each(function(){
+		
+		if($(this)!=this_var && $(this).parent().hasClass("is-more")){
+			
+			$(this).parent().removeClass("is-more").find(".hide-el").slideUp(350);
+			$(this).find(".frame-video").html("");
+			
+		}
+	});
+	
+	$(this).parent().addClass("is-more").find(".hide-el").slideDown(350);
+	
+		$(this).find(".frame-video").html("<iframe src='https://www.youtube.com/embed/" + $(this).find(".frame-video").attr("data-video") + "' frameborder='0' allowfullscreen></iframe>");
+	
+		$('html, body').animate({scrollTop: $(this).offset().top}, 2000);
+		
+	}
+});
+}
