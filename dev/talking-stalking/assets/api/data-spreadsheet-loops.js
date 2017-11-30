2ce876d5aa97872f9cb59bda49c3761903bf1acc
lab@thequint.com <shahadat.hussain@thequint.com>
2017-11-30 12:12:30 +0530

    Make dynamic Testimonials Signature

--- dev/talking-stalking/assets/api/data-spreadsheet-loops.js
+++ dev/talking-stalking/assets/api/data-spreadsheet-loops.js
@@ -1,5 +1,7 @@
-var public_spreadsheet_contact = 'https://docs.google.com/spreadsheets/d/1sjjvoZysqGBvEA3vTPbumJJPhZmphu4vDqXR3U9hahU/pubhtml';
-	
+// var public_spreadsheet_contact = 'https://docs.google.com/spreadsheets/d/1sjjvoZysqGBvEA3vTPbumJJPhZmphu4vDqXR3U9hahU/pubhtml';
+
+var public_spreadsheet_contact = 'https://docs.google.com/spreadsheets/d/1sionjEKyqPwgje294nBdPiK90NxH4B0bW2Je2u_gRwk/pubhtml';
+
 function init(){
 	Tabletop.init({
 		key: public_spreadsheet_contact,
@@ -11,53 +13,30 @@ function init(){
 window.addEventListener('DOMContentLoaded', init);
 
 function showInfo(data) {
-	//console.log(data[0].Comments);
-	$("#total_length").html(data.length);
-	for(var index = data.length-1; index >= 0; index--) {
-		
-		$("#testimonials-first").append (
-			"<div class='block-content'>"
-				+ "<div class=''>" +data[index].Testimonial_First +"</div>"
-			+"</div>"
-		),
-		$("#testimonials-second").append (
-			"<div class='block-content'>"
-				+ "<div class=''>" +data[index].Testimonial_Second +"</div>"
-			+"</div>"
-		),
-		$("#testimonials-third").append (
-			"<div class='block-'>"
-				+ "<div class=''>" +data[index].Testimonial_Third +"</div>"
-			+"</div>"
-		)
-		// if(data[index].Reply == "undefined" || data[index].Reply == "") {
-		// 	$("#display_data").append (
-		// 		"<div class='block-content'>"
-		// 			+"<div class='que-block'>"
-		// 				+ "<span class='author'>" +data[index].Name +"</span>"
-		// 				+ "<span class='timestamp'>" +data[index].Email +"</span>"
-		// 				+ "<p class='comment'>" +data[index].Comments +"</p>"
-		// 			+"</div>"
-		// 		+"</div>"
-		// 	)
-		// }else {
-		// 	$("#display_data").append (
-		// 		"<div class='block-content'>"
-		// 			+"<div class='que-block'>"
-		// 				+ "<div class='avtar'><img src='assets/images/avatar2.jpg'></div>"
-		// 				+ "<span class='author'>" +data[index].Name +"</span>"
-		// 				+ "<span class='timestamp'>" +data[index].Timestamp +"</span>"
-		// 				+ "<p class='comment'>" +data[index].Comments +"</p>"
-		// 			+"</div>"
 
-		// 			+"<div class='ans-block'>"
-		// 				+ "<div class='avtar'><img src='assets/images/avatar.jpg'></div>"
-		// 				+ "<span class='author'>TheQuint</span>"
-		// 				+ "<span class='timestamp'>" +data[index].Timestamp +"</span>"
-		// 				+ "<p class='comment'>" +data[index].Reply +"</p>"
-		// 			+"</div>"
-		// 		+"</div>"
-		// 	)
-		// }
-	}
+	console.log(data[0].Total_Signatures);
+	$(".Total_Testimonials").append (
+		data[0].Total_Testimonials
+	),
+	$(".Total_Signatures").append (
+		data[0].Total_Signatures
+	)
+	//console.log(data[0].Comments);
+	// $("#total_length").html(data.length);
+	// for(var index = data.length-1; index >= 0; index--) {
+		
+	// 	$("#Total_Testimonials").append (
+	// 		+data[index].Total_Testimonials
+	// 	),
+	// 	$("#testimonials-second").append (
+	// 		"<div class='block-content'>"
+	// 			+ "<div class=''>" +data[0].Total_Signatures +"</div>"
+	// 		+"</div>"
+	// 	),
+	// 	$("#testimonials-third").append (
+	// 		"<div class='block-'>"
+	// 			+ "<div class=''>" +data[index].Testimonial_First +"</div>"
+	// 		+"</div>"
+	// 	)
+	// }
 }
