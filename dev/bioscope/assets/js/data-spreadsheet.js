25aaa4bcc7e83034bd5c78a457aa0a2359a836c6
Achintya De <achintya.de@thequint.com>
2017-12-04 15:22:35 +0530

    update

--- dev/bioscope/assets/js/data-spreadsheet.js
+++ dev/bioscope/assets/js/data-spreadsheet.js
@@ -1,169 +1,89 @@
-var public_spreadsheet_contact = 'https://docs.google.com/spreadsheets/d/1TYaNIvwavphflUqrGqlZ7jt1JTjt-MAwwFdsEAnAinY/pubhtml';
+var public_spreadsheet_contact = 'https://docs.google.com/spreadsheets/d/1BEnGjJDIAW_LdOKsBtVd_RdlPORnfC-P-gg1RIgSPJU/pubhtml';
 
-//demo_sheet
-//var public_spreadsheet_contact = 'https://docs.google.com/spreadsheets/d/18RrUpQs43I_FfdmqUYHoocnqF8Y2Tt6FYp6D_fc7iT4/pubhtml';
 
 function init() {
-   Tabletop.init({
-      key: public_spreadsheet_contact,
-      callback: showInfo,
-      simpleSheet: true
-   });
+	Tabletop.init({
+		key: public_spreadsheet_contact,
+		callback: showInfo,
+		simpleSheet: true
+	});
 }
 
 window.addEventListener('DOMContentLoaded', init);
 
-var sheet_data, sheet_data_all;
-var gender_filter = "all";
-var category_filter = "all";
+var sheet_data;
+
 
 
 function getParameterByName(name, url) {
-   if (!url) url = window.location.href;
-   name = name.replace(/[\[\]]/g, "\\$&");
-   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-   if (!results) return null;
-   if (!results[2]) return '';
-   return decodeURIComponent(results[2].replace(/\+/g, " "));
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 function showInfo(data) {
 
-   sheet_data = data;
-   sheet_data_all = data;
-   display_data();
+	sheet_data = data;
 	
- document.getElementById("countVoice").innerHTML = "<span>" + [sheet_data[0].Total_Voices] + ' Voices'+"</span>";	
-	
-	//console.log([sheet_data[0].Total_Voices]);
+	display_data();
 
 }
 
 
 function display_data() {
+
+	for (var index = sheet_data.length - 1; index >= 0; index--) {
+
+		$("#SwiperItems").append("<li class='swiper-slide' data-year='" + sheet_data[index].Year + "' data-description='" + sheet_data[index].Description + "' data-video='" + sheet_data[index].Video + "' data-discussion_1='" + sheet_data[index].Discussion_1 + "' data-discussion_2='" + sheet_data[index].Discussion_2 + "'><a href='javascript:void(0);'><img src='" + sheet_data[index].Image + "' alt='" + sheet_data[index].Name + "'></a></li>");
+		
+		
+	}
+
+	Swiper_Slider();
 	
-	
-	
-   $("#listData").html("");
-   for (var index = sheet_data.length - 1; index >= 0; index--) {
-
-      var cat_class;
-
-      switch (sheet_data[index].Category) {
-         case "GST":
-            cat_class = "ctg-orange";
-            break;
-         case "Education":
-            cat_class = "ctg-blue";
-            break;
-         case "Healthcare":
-            cat_class = "ctg-turquoise";
-            break;
-         case "Prohibition":
-            cat_class = "ctg-limegreen";
-            break;
-         case "Unemployment":
-            cat_class = "ctg-violet";
-            break;
-         case "Vikas":
-            cat_class = "ctg-pink";
-            break;
-         case "Others":
-            cat_class = "ctg-red";
-            break;
-			  
-		case "Story":
-            cat_class = "ctg-story";
-            break;
-
-         default:
-            cat_class = "";
-
-
-      }
-  
-	   if(cat_class=="ctg-story")
-		   {
-			  $("#listData").append("<li class='" + cat_class + "'><p><a href='"+ sheet_data[index].Story_URL +"' target='_blank'><span>" + sheet_data[index].Description + "</span></a></p></li>"); 
-		   }
-	   else
-		   {
-	   $("#listData").append("<li class='" + cat_class + "'><figure class='chat-el'><div class='show-el'><div class='profile-img'><img src='" + sheet_data[index].Profile_Image + "' alt='" + sheet_data[index].Name + "'></div><div class='msg'><div class='msg-text'><blockquote>" + sheet_data[index].Sub_Headline + "</blockquote><a class='btn-more' alt='Gujarat election 2017 - " + sheet_data[index].Alt_Tag + "' href='javascript:void(0);'>Read More</a></div></div></div><div class='hide-el'><div class='frame-video' data-video='" + sheet_data[index].Video + "'></div><div class='personal-details'><h4><label>Name: </label>" + sheet_data[index].Name + "</h4><h4><label>Age: </label>" + sheet_data[index].Age + "</h4><h4><label>Location: </label>" + sheet_data[index].Location + "</h4><p>" + sheet_data[index].Description + "</p></div></div></figure></li>");
+	    var Display_Name = $('.swiper-slide-active img').attr('alt');
+	    var Display_Year = $('.swiper-slide-active').attr('data-year');
+	    var Display_Description = $('.swiper-slide-active').attr('data-description');
+		var Display_Video = $('.swiper-slide-active').attr('data-video');
+	    var Display_Discussion_1 = $('.swiper-slide-active').attr('data-discussion_1');
+	    var Display_Discussion_2 = $('.swiper-slide-active').attr('data-discussion_2');
 	   
-		   }
-   }
-   moreData();
+	
+		//console.log(Display_Video);
+		
+		$('#DisplayName').html(Display_Name);
+		$('#DisplayYear').html(Display_Year);
+		$('#DisplayDescription').html(Display_Description);
+	    $('#DisplayVideo').html("<iframe src='https://www.youtube.com/embed/" + Display_Video + "' frameborder='0' allowfullscreen></iframe>");
+		$('#DisplayDiscussion_1').html(Display_Discussion_1);
+	    $('#DisplayDiscussion_2').html(Display_Discussion_2);
+	
+	
+	$('.swiper-button').click(function(){
+		var Display_Name = $('.swiper-slide-active img').attr('alt');
+		var Display_Year = $('.swiper-slide-active').attr('data-year');
+		var Display_Description = $('.swiper-slide-active').attr('data-description');
+		var Display_Video = $('.swiper-slide-active').attr('data-video');
+		var Display_Discussion_1 = $('.swiper-slide-active').attr('data-discussion_1');
+	    var Display_Discussion_2 = $('.swiper-slide-active').attr('data-discussion_2');
+		
+		
+		$('#DisplayName').html(Display_Name);
+		$('#DisplayYear').html(Display_Year);
+		$('#DisplayDescription').html(Display_Description);
+		$('#DisplayVideo').html("<iframe src='https://www.youtube.com/embed/" + Display_Video + "' frameborder='0' allowfullscreen></iframe>");
+		$('#DisplayDiscussion_1').html(Display_Discussion_1);
+	    $('#DisplayDiscussion_2').html(Display_Discussion_2);
+		
+		
+	});
+	
+	
+	
+	
+	//console.log($('.swiper-slide-active img').attr('alt'));
 }
-
-
-$(".filter-block .options li").click(function() {
-
-   sheet_data = []
-
-
-   if ($(this).hasClass("is-active")) {
-      category_filter = "all";
-      $(".filter-block .options li").removeClass("is-active");
-      $(this).removeClass("is-active");
-      $(".clear-filter").removeClass("is-filter");
-   } else {
-      category_filter = $(this).attr("data-category");
-      $(".filter-block .options li").removeClass("is-active");
-      $(this).addClass("is-active");
-      $(".clear-filter").addClass("is-filter");
-   }
-
-
-   for (var index = 0; index <= sheet_data_all.length - 1; index++) {
-
-      if ((sheet_data_all[index].Category == category_filter || category_filter == "all") && ((gender_filter == "all") || gender_filter == sheet_data_all[index].Sex)) {
-         sheet_data.push(sheet_data_all[index]);
-      }
-   }
-   display_data();
-});
-
-
-$(".filter-block .gender li.gender-item").click(function() {
-
-   if ($(this).hasClass("is-active")) {
-      gender_filter = "all";
-      $(".filter-block .gender li.gender-item").removeClass("is-active");
-      $(this).removeClass("is-active");
-      $(".clear-filter").removeClass("is-filter");
-
-   } else {
-      $(".filter-block .gender li.gender-item").removeClass("is-active");
-      $(this).addClass("is-active");
-      gender_filter = $(this).attr("data-gender");
-      $(".clear-filter").addClass("is-filter");
-   }
-   sheet_data = []
-   for (var index = 0; index <= sheet_data_all.length - 1; index++) {
-
-      if ((sheet_data_all[index].Category == category_filter || category_filter == "all") && ((gender_filter == "all") || gender_filter == sheet_data_all[index].Sex)) {
-         sheet_data.push(sheet_data_all[index]);
-      }
-
-   }
-   display_data();
-
-});
-
-
-$(".clear-filter").click(function() {
-
-   sheet_data = sheet_data_all;
-   gender_filter = "all";
-   category_filter = "all";
-   display_data();
-   $(".filter-block .gender li.gender-item").removeClass("is-active");
-   $(this).removeClass("is-filter");
-   $(".filter-block .options li").removeClass("is-active");
-});
-
-
-
-
-
--- dev/bioscope/assets/js/functions.js
+++ dev/bioscope/assets/js/functions.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+ function Swiper_Slider(){
 	var swiper = new Swiper('.swiper-container', {
 		effect: 'coverflow',
 		grabCursor: true,
-		//centeredSlides: true,
+		centeredSlides: true,
 		slidesPerView: 'auto',
 		slidesPerView: 3,
 		loop: true,
@@ -19,40 +19,7 @@ $(document).ready(function () {
 		prevEl: '.swiper-button-prev',
 		}
 	});
-});
-
-
-
-
- function moreData(){
-
-$(".chat-el").click(function(){
-	
-	if($(this).parent().hasClass("is-more")){
-		$(this).parent().removeClass("is-more").find(".hide-el").slideUp(350);
-		$(this).find(".frame-video").html("");
-	}
-	
-	else {
-		
-	var this_var = $(this);	
-		
-	$(".chat-el").each(function(){
-		
-		if($(this)!=this_var && $(this).parent().hasClass("is-more")){
-			
-			$(this).parent().removeClass("is-more").find(".hide-el").slideUp(350);
-			$(this).find(".frame-video").html("");
-			
-		}
-	});
-	
-	$(this).parent().addClass("is-more").find(".hide-el").slideDown(350);
-	
-		$(this).find(".frame-video").html("<iframe src='https://www.youtube.com/embed/" + $(this).find(".frame-video").attr("data-video") + "' frameborder='0' allowfullscreen></iframe>");
-	
-		$('html, body').animate({scrollTop: $(this).offset().top}, 2000);
-		
-	}
-});
+	 
+	 $('*').removeClass('loading');
 }
+
