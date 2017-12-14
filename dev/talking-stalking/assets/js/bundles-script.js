6c2ca4ce37d18e301c5b2b8362be520186048ad5
lab@thequint.com <shahadat.hussain@thequint.com>
2017-12-14 14:55:47 +0530

    Minify JS and CSS

--- dev/talking-stalking/assets/js/bundles-script.js
+++ dev/talking-stalking/assets/js/bundles-script.js
@@ -775,10 +775,14 @@ function handleTweets(tweets) {
    }, 10000);
 }
 twitterFetcher.fetch(config5);
-;
-$(".intro-close").click(function(e) {
-    $(".intro-popup").fadeOut();
+;$(".intro-close").click(function(e) {
+    $(".intro-popup").hide();
     $('.intro-iframe iframe').attr('src', '');
+    $(".scroll-up").show();
+
+    setTimeout(function(){
+        $(".scroll-up").fadeOut();
+    }, 3000);
 });
 
 $(".popup-btn").click(function(e) {
@@ -944,6 +948,11 @@ var cur_section = 0;
 var click_flag = 0;
 
 function repeatOften() {
+
+    if($(window).scrollTop()>0) {
+        $(".scroll-up").fadeOut(100);
+    }
+    
     if (-1 * parseInt($("#sectipn_wrap").css("left")) > 0 && -1 * parseInt($("#sectipn_wrap").css("left")) < $(window).width()) {
         cur_section = 0;
     } else if (-1 * parseInt($("#sectipn_wrap").css("left")) > $(window).width() && -1 * parseInt($("#sectipn_wrap").css("left")) < 2 * $(window).width()) {
@@ -1022,6 +1031,10 @@ $(window).load(function() {
     $(".loader").fadeOut();
     //v_slider();
 });
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 
 $(window).resize(function() {
     init();
