a00e491720ca74d10a4dd990b59e8eaeb1313280
Achintya De <achintya.de@thequint.com>
2018-01-02 13:16:47 +0530

    updated path

--- dev/embeds/data-spreadsheet.js
+++ dev/embeds/data-spreadsheet.js
@@ -16,6 +16,15 @@ window.addEventListener('DOMContentLoaded', init);
 function showInfo(data) {	
 document.getElementById("matchcentre_html").innerHTML = "<div class='simc-quint-container' id='siMtcContainer' data-matchcode='"+  data[0].MatchId +"'></div>";
 	
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://assets-quint.sportz.io/cricket/widgets/ipl-masthead/js/score-widget-init.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+
+
+	
 	
 	
 	
