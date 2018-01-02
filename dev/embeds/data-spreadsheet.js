f63ea6a64b1e4fbc0e993a9fb6a2b68620e3e80e
Achintya De <achintya.de@thequint.com>
2018-01-02 12:46:05 +0530

    update

---
+++ dev/embeds/data-spreadsheet.js
@@ -0,0 +1,31 @@
+
+var public_spreadsheet_elections = 'https://docs.google.com/spreadsheets/d/1zn7CbWMNkQ0pwNsNSw89IeMtGR_gv4zKLqwVdXJ5-0E/pubhtml';
+	
+function init(){
+Tabletop.init({
+	key: public_spreadsheet_elections,
+	callback: showInfo,
+	simpleSheet: true 
+});
+}
+
+
+
+window.addEventListener('DOMContentLoaded', init);
+
+function showInfo(data) {	
+document.getElementById("matchcentre_html").innerHTML = "<div class='simc-quint-container' id='siMtcContainer' data-matchcode='"+ [ data[0].MatchId,] +"'></div>";
+	
+	
+	
+	
+
+	
+console.log(data);
+}
+
+
+
+
+
+
