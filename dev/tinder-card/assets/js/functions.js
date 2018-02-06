b26981dccf94320fe0db69768a3bbc5e52dddfc6
Achintya De <achintya.de@thequint.com>
2018-02-06 14:15:45 +0530

    update

--- dev/tinder-card/assets/js/functions.js
+++ dev/tinder-card/assets/js/functions.js
@@ -42,6 +42,7 @@ function action_click(arg){
 	$('.reload').addClass('is-show');
 	$('.dislike').addClass('is-hide');
 	$('.like').addClass('is-hide');	
+	$('#status').addClass('is-front');		
 	
 	if(arg==0)//dislike
 		{
