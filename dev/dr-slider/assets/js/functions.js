560174b2e8be6764b3c24527e1b3b47b5076968d
Achintya De <achintya.de@thequint.com>
2018-02-09 16:37:59 +0530

    update

---
+++ dev/dr-slider/assets/js/functions.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	
+	$('#DrSlider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		arrows: false,
+		autoplaySpeed: 2000,
+		dots: false,
+		
+		responsive: [
+
+			{
+				breakpoint: 1279,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 960,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					variableWidth:true,
+					
+				}
+			}
+
+		]
+	});
+});
