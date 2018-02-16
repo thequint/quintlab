3f5f3a062c30648fd2a5c7cb37e8f8929f5e2ba8
lab@thequint.com <shahadat.hussain@thequint.com>
2018-02-16 11:44:25 +0530

    Bug fixes

--- dev/switch/mp-tourism/assets/js/functions.js
+++ dev/switch/mp-tourism/assets/js/functions.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
+  	// $(".btn-01").on("click",function(){
+   //   	$(".box-first").hide();
+   //   	$(".box-second").show();
+  	// });
+  	// $(".btn-02").on("click",function(){
+   //   	$(".box-first").show();
+   //   	$(".box-second").hide();
+  	// });
   	$(".btn-01").on("click",function(){
-     	$(".box-first").hide();
-     	$(".box-second").show();
+  		$(".box-wrap").addClass("slide");
   	});
   	$(".btn-02").on("click",function(){
-     	$(".box-first").show();
-     	$(".box-second").hide();
+  		$(".box-wrap").removeClass("slide");
   	});
+
   	//$('.content').css("background","lightgreen");
 });
 
+//$(".main-wrap--container").addClass("slide");
 
 // $('.progress').addClass("filling").css("background","lightgreen").width("100%");
