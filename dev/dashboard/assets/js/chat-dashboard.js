269634048711f272e7eb429e903cb4aa825a8923
Achintya De <achintya.de@thequint.com>
2018-02-20 18:17:19 +0530

    update

---
+++ dev/dashboard/assets/js/chat-dashboard.js
@@ -0,0 +1,302 @@
+// https://pbs.twimg.com/profile_images/506946032421199872/eqF3gv7d.jpeg
+// https://media-exp2.licdn.com/mpr/mpr/shrinknp_200_200/p/4/000/13a/1a6/077024b.jpg
+
+
+$('input').val('');
+
+// Create profile
+
+init();
+
+var single_profile, profile_thumb;
+
+single_profile = ('<li><a class="remove-profile" href="javascript:void(0);"><i class="fa fa-times"></i></a><figure><div class="profile-img"><img alt="" src="assets/images/avatar.svg"></div><figcaption><input type="text" name="Profile Name" class="form-control name"  placeholder="Enter profile Name"><input type="text" name="Profile Image" class="form-control url" placeholder="Enter Image Url"></figcaption></figure></li>');
+
+profile_thumb = ('<li><a href="javascript:void(0);"><figure><div class="profile-img"><img alt="" src="assets/images/avatar.svg"></div><figcaption class="profle-des"></figcaption></figure></a></li>');
+
+
+$('#BtnAddProfile').click(function () {
+	"use strict";
+	$('#ProfileDetails').append(single_profile);
+	$('#ProfileList').append(profile_thumb);
+	init();
+});
+
+function reset_index() {
+	"use strict";
+	$('#ProfileDetails li').each(function (i) {
+
+		$(this).attr("data-index", (i + 1));
+
+	});
+
+	$('#ProfileList li').each(function (i) {
+
+		$(this).attr("data-index", (i + 1));
+
+	});
+}
+
+// update profile image
+
+
+function update_profile_images(this_var) {
+
+	if (this_var.val() === '') {
+
+		this_var.closest("li").find('.profile-img img').attr('src', 'assets/images/avatar.svg');
+
+
+	} else {
+
+		this_var.closest("li").find('.profile-img img').attr('src', this_var.val());
+	}
+
+
+	$("#ProfileList li, #MessageBulder li").each(function (index) {
+
+		if ($(this).attr('data-index') === this_var.closest("li").attr("data-index")) {
+			$(this).find("img").attr('src', this_var.closest("li").find('.profile-img img').attr('src'));
+			$(this).find('.profile-img img').attr('src', this_var.closest("li").find('.profile-img img').attr('src'));
+
+
+		}
+
+	});
+}
+
+
+function update_profile_name(this_var) {
+
+	if (this_var.val() === '') {
+
+		this_var.closest("li").find('.profile-img img').attr('alt', '');
+
+
+
+	} else {
+
+		this_var.closest("li").find('.profile-img img').attr('alt', this_var.val());
+	}
+
+
+	$("#ProfileList li, #MessageBulder li").each(function (index) {
+
+		if ($(this).attr('data-index') === this_var.closest("li").attr("data-index")) {
+			$(this).find(".profle-des").text(this_var.closest("li").find('.name').val());
+			$(this).find('.profile-img img').attr('alt', this_var.closest("li").find('.name').val());
+		}
+
+
+	});
+}
+
+
+$('#ProfileDetails li .url').bind("keyup change", function () {
+
+	update_profile_images($(this))
+});
+
+
+
+
+$('#ProfileDetails li .name').bind("keyup change", function () {
+
+	update_profile_name($(this))
+});
+
+
+
+
+
+
+// Remove Only Message
+
+function remove_message() {
+	$('#MessageBulder li .remove-profile').click(function () {
+		$(this).closest("li").remove();
+	});
+}
+
+
+
+$('#ProfileList li:first-child').click(function () {
+	message_bulder($(this).attr('data-index'));
+	remove_message();
+});
+
+
+
+
+// message bulder
+
+function message_bulder(index) {
+	"use strict";
+	$('.message-bulder-section').show();
+
+
+	var profile_msz;
+	var profile_img = '';
+	var profile_img_src = $("#ProfileList li:nth-child(" + index + ")").find("img").attr('src');
+	//console.log($("#ProfileList li:nth-child(" + index + ")").find("img").attr('src'));
+	// if li length is zero assign left
+	if ($('#MessageBulder li').length === 0) {
+		profile_msz = '<li data-index="' + index + '" class="message-left">';
+		profile_img = ' <div class="profile-img"><img alt="avatar" src="' + profile_img_src + '"></div>';
+	} else {
+		// if li last child data-index is same as index then assign same class as last child
+		if ($('#MessageBulder li:last-child').attr('data-index') === index) {
+			// if last child has class left assign left
+
+			if ($('#MessageBulder li:last-child').hasClass('message-left')) {
+				profile_msz = '<li data-index="' + index + '" class="message-left">';
+			}
+
+			// else assign right
+			else {
+				profile_msz = '<li data-index="' + index + '" class="message-right">';
+			}
+
+
+		} else {
+
+			if ($('#MessageBulder li:last-child').hasClass('message-left'))
+			// if last child has class left assign right
+			{
+				profile_msz = '<li data-index="' + index + '" class="message-right">';
+				profile_img = ' <div class="profile-img"><img alt="avatar" src="' + profile_img_src + '"></div>';
+			}
+
+			// else assign left
+			else {
+				profile_msz = '<li data-index="' + index + '" class="message-left">';
+				profile_img = ' <div class="profile-img"><img alt="avatar" src="' + profile_img_src + '"></div>';
+			}
+
+		}
+	}
+
+
+	profile_msz += '<a class="remove-profile" href="javascript:void(0);"><i class="fa fa-times"></i></a><figure>' + profile_img + '<figcaption><textarea placeholder="Paste the embed code here" class="form-control"></textarea></figcaption></figure></li>';
+
+	$('#MessageBulder').append(profile_msz);
+
+}
+
+
+// All init
+
+function init() {
+	"use strict";
+	reset_index();
+
+	$("#ProfileDetails li:last-child").find('.remove-profile').click(function () {
+
+		var this_data_index = $(this).closest('li').attr('data-index');
+		//alert(this_data_index);
+
+		$(this).closest("li").remove();
+
+		$("#ProfileList li").each(function () {
+
+			if ($(this).closest('li').attr('data-index') === this_data_index) {
+				$(this).remove();
+			}
+		});
+
+		reset_index();
+
+	});
+
+	$('#ProfileDetails li:last-child .url').bind("keyup change", function () {
+
+		update_profile_images($(this));
+
+	});
+
+
+	$('#ProfileDetails li:last-child .name').bind("keyup change", function () {
+
+		update_profile_name($(this));
+
+	});
+
+
+	$('#ProfileList li:last-child').click(function () {
+
+		message_bulder($(this).attr('data-index'));
+
+		remove_message();
+	});
+
+	$('#ProfileDetails li .remove-profile').click(function () {
+
+		//console.log("yes");
+
+		var profile_data_index = $(this).closest('li').attr('data-index');
+
+		$('#MessageBulder li').each(function () {
+
+			var this_data_index = $(this).attr('data-index');
+
+			//console.log(profile_data_index +"|"+ this_data_index)
+			if (profile_data_index === this_data_index) {
+
+				$(this).remove();
+
+			}
+
+
+		});
+
+
+
+	});
+
+
+}
+
+
+chat_data = {};
+
+function build_array() {
+
+
+	chat_data['profiles'] = [];
+	chat_data['discussion'] = [];
+
+	$("#ProfileDetails li").each(function (index) {
+		chat_data['profiles'].push({});
+
+		chat_data['profiles'][index]['profile_name'] = $(this).find(".name").val();
+
+		if($(this).find(".url").val() === '')
+		{
+			chat_data['profiles'][index]['img_url'] = 'assets/images/avatar.svg';
+		}
+		else
+		{
+			chat_data['profiles'][index]['img_url'] = $(this).find(".url").val();
+		}
+	});
+
+	$("#MessageBulder li").each(function (index) {
+
+		chat_data['discussion'].push({});
+
+		chat_data['discussion'][index]['text'] = $(this).find("textarea").val();
+
+		chat_data['discussion'][index]['profile'] = $(this).attr('data-index');
+
+	});
+
+	preview_data();
+
+}
+
+
+$('#BtnPreview').click(function () {
+	"use strict";
+	build_array();
+	console.log(chat_data);
+});
--- dev/dashboard/assets/js/chat.js
+++ dev/dashboard/assets/js/chat.js
@@ -1,153 +1,54 @@
-var chatArray = [], messageArray = [], errorArray = [];
-
-$(function(){
-	$('.player-buttons').hide();
-	$('.message-bulder-section').hide();
-	$('.player-left #colorSelector').ColorPicker({
-		color: '#0000ff',
-		onShow: function (colpkr) {
-			$(colpkr).fadeIn(500);
-			return false;
-		},
-		onHide: function (colpkr) {
-			$(colpkr).fadeOut(500);
-			return false;
-		},
-		onChange: function (hsb, hex, rgb) {
-			$('.player-left #colorSelector div').css('backgroundColor', '#' + hex);
-			// $('#player-name-input').css('backgroundColor', '#' + hex);
-			// $('.player-name').css('backgroundColor', '#' + hex);
-		}
-	});
-
-	$('.player-right #colorSelector').ColorPicker({
-		color: '#0000ff',
-		onShow: function (colpkr) {
-			$(colpkr).fadeIn(500);
-			return false;
-		},
-		onHide: function (colpkr) {
-			$(colpkr).fadeOut(500);
-			return false;
-		},
-		onChange: function (hsb, hex, rgb) {
-			$('.player-right #colorSelector div').css('backgroundColor', '#' + hex);
-		}
-	});
-	$('.message-left #colorSelector').ColorPicker({
-		color: '#0000ff',
-		onShow: function (colpkr) {
-			$(colpkr).fadeIn(500);
-			return false;
-		},
-		onHide: function (colpkr) {
-			$(colpkr).fadeOut(500);
-			return false;
-		},
-		onChange: function (hsb, hex, rgb) {
-			$('.message-left #colorSelector div').css('backgroundColor', '#' + hex);
-		}
-	});
-
-	$('.message-right #colorSelector').ColorPicker({
-		color: '#0000ff',
-		onShow: function (colpkr) {
-			$(colpkr).fadeIn(500);
-			return false;
-		},
-		onHide: function (colpkr) {
-			$(colpkr).fadeOut(500);
-			return false;
-		},
-		onChange: function (hsb, hex, rgb) {
-			$('.message-right #colorSelector div').css('backgroundColor', '#' + hex);
-		}
-	});
-});
-
-var playerLeft = '<div class="player-left clearfix"><div class="player-image"><form><input type="text" name="playerImage" class="player-image-input" id="player-image-input" placeholder="Enter image Url....."><label for="player-image-input"><i class="fas fa-user-plus"></i></label></form></div><div class="player-name"><div class="row"><div class="col-lg-10"><form><input type="text" name="playerName" class="player-name-input" id="player-name-input" placeholder="Enter name of the player"></form></div><div class="col-lg-2"><div id="colorSelector"><div style="background-color: purple"></div></div></div></div></div></div>';
-
-var playerRight = '<div class="player-right clearfix"><div class="player-name"><div class="row"><div class="col-lg-2"><div id="colorSelector"><div style="background-color: yellow"></div></div></div><div class="col-lg-10"><form><input type="text" name="playerName" class="player-name-input" id="player-name-input" placeholder="Enter name of the player"></form></div></div></div><div class="player-image"><form><input type="text" name="playerImage" class="player-image-input" id="player-image-input" placeholder="Enter image Url....."><label for="player-image-input"><i class="fas fa-user-plus"></i></label></form></div></div>';
-
-var messageLeft = '<div class="message-left clearfix"><div class="player-image"><img alt="" src="assets/images/userIcon.png"></div><div class="message-text"><div class="row"><div class="col-lg-12"><form><input type="text" name="messageText" class="message-text-input" id="message-text-input" placeholder="Paste the embed code here..."></form></div></div></div></div>';
-
-var messageRight = '<div class="message-right clearfix"><div class="message-text"><div class="row"><div class="col-lg-12"><form><input type="text" name="messageText" class="message-text-input" id="message-text-input" placeholder="Paste the embed code here..."></form></div></div></div><div class="player-image"><img alt="" src="assets/images/userIcon.png"></div></div>';
-
-var messageLeftWithoutImage = '<div class="message-left clearfix"><div class="message-text"><div class="row"><div class="col-lg-12"><form><input type="text" name="messageText" class="message-text-input" id="message-text-input" placeholder="Paste the embed code here..."></form></div></div></div></div>';
-
-var messageRightWithoutImage = '<div class="message-right clearfix"><div class="message-text"><div class="row"><div class="col-lg-12"><form><input type="text" name="messageText" class="message-text-input" id="message-text-input" placeholder="Paste the embed code here..."></form></div></div></div></div>';
-
-$('.start-btn').click(function(){
-	$('.player-buttons').show();
-	$('.message-bulder-section').show();
-});
-
-$('.player1').click(function(){
-	$('.message-bulder-section').append(messageLeft);
-});
-
-$('.player2').click(function(){
-	$('.message-bulder-section').append(messageRight);
-});
-
-function addPlayersToArray(){
-	var i = 0;
-	$('.player-left, .player-right').each(function(){
-		chatArray.push({});
-		chatArray[i]['name'] = $(this).find('.player-name-input').val();
-		chatArray[i]['imageUrl'] = $(this).find('.layer-image-input').val();
-		chatArray[i]['color'] = $(this).find('#colorSelector div').css('background-color');
-		i++;
-	});
-}
-
-
-function addMessagesToArray(){
-	var i = 0;
-	$('.message-left, .message-right').each(function(){
-		chatArray.push({});
-		messageArray.push({});
-		messageArray[i]['name'] = $(this).find('.player-name-input').val();
-		messageArray[i]['imageUrl'] = $(this).find('.layer-image-input').val();
-		messageArray[i]['color'] = $(this).find('#colorSelector div').css('background-color');
-		i++;
-	});
-	chatArray[chatArray.length - 1]['messages'] = messageArray;
-	messageArray = [];
-}
-
-function buildArray(){
-	var errorNum = validates();
-	if(errorNum == 0){
-		chatArray = [];
-		addPlayersToArray();
-		addMessagesToArray();
-	}
-	else{
-		alert(errorArray[errorNum]);
-		return;
-	}
-}
-
-function validates(){
-
-}
-
-$('.refresh-icon').click(function(){
-	if(chatArray == []){
-		buildArray();
-		buildPreview();
-	}
-	else{
-		buildPreview();
-	}
-});
-
-function buildPreview(){
-	var prevPlayer = '';
+function preview_data() {
 	$('#preview').html('');
-	for(var i = 0; i < chatArray[chatArray.length].length; i++){
+	var profile_msz;
+	var profile_img = '';
+	var profile_img_src = '';
+	for (var i = 0; i < chat_data.discussion.length; i++) {
 
-		$('#preview').append('');
+		if ($('#preview li').length === 0) {
+			//console.log(chat_data.discussion[i].profile - 1);
+			profile_msz = '<li data-index="' + chat_data.discussion[i].profile + '" class="message-left">';
+			profile_img = ' <div class="profile-img"><img alt="' + chat_data.profiles[chat_data.discussion[i].profile - 1].profile_name
+ + '" src="' + chat_data.profiles[chat_data.discussion[i].profile - 1].img_url + '"></div>';
+		} else {
+			// if li last child data-index is same as index then assign same class as last child
+			if ($('#preview li:last-child').attr('data-index') === chat_data.discussion[i].profile) {
+				// if last child has class left assign left
+
+				if ($('#preview li:last-child').hasClass('message-left')) {
+					profile_msz = '<li data-index="' + chat_data.discussion[i].profile + '" class="message-left">';
+				}
+
+				// else assign right
+				else {
+					profile_msz = '<li data-index="' + chat_data.discussion[i].profile + '" class="message-right">';
+				}
+
+
+			} else {
+
+				if ($('#preview li:last-child').hasClass('message-left'))
+				// if last child has class left assign right
+				{
+					profile_msz = '<li data-index="' + chat_data.discussion[i].profile + '" class="message-right">';
+					profile_img = ' <div class="profile-img"><img alt="' + chat_data.profiles[chat_data.discussion[i].profile - 1].profile_name
+ + '" src="' + chat_data.profiles[chat_data.discussion[i].profile - 1].img_url + '"></div>';
+				}
+
+				// else assign left
+				else {
+					profile_msz = '<li data-index="' + chat_data.discussion[i].profile + '" class="message-left">';
+					profile_img = ' <div class="profile-img"><img alt="' + chat_data.profiles[chat_data.discussion[i].profile - 1].profile_name
+ + '" src="' + chat_data.profiles[chat_data.discussion[i].profile - 1].img_url + '"></div>';
+				}
+
+			}
+		}
+
+		profile_msz += '<figure>' + profile_img + '<figcaption>' + chat_data.discussion[i].text + '</figcaption></figure></li>';
+
+		$('#preview').append(profile_msz);
+
 	}
+
 }
