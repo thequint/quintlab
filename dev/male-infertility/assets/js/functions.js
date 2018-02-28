672ae75924ae67ece69e2545ee40d3d5529588d9
Achintya De <achintya.de@thequint.com>
2018-02-28 18:47:15 +0530

    update

---
+++ dev/male-infertility/assets/js/functions.js
@@ -0,0 +1,12 @@
+
+
+
+$(document).ready(function(){
+	$('.assets a').click(function(){
+		$('.modal-outer').show();
+	});
+	
+	$('.modal-close').click(function(){
+		$('.modal-outer').hide();
+	});
+});
