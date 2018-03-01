fd59fe46675966d3d7234a9569107b4b405b3767
Achintya De <achintya.de@thequint.com>
2018-03-01 16:55:34 +0530

    up

--- dev/male-infertility/assets/js/functions.js
+++ dev/male-infertility/assets/js/functions.js
@@ -2,11 +2,20 @@
 
 
 $(document).ready(function(){
-	$('.assets a').click(function(){
+	$('.assets a path,.assets a circle,.assets a polygon,.assets a text,.assets a rect').click(function(){
 		$('.modal-outer').show();
 	});
 	
 	$('.modal-close').click(function(){
 		$('.modal-outer').hide();
 	});
+	
+	$("path,circle,polygon,text,rect").mouseenter(function(){
+		console.log("hover");
+		$(this).closest("a").addClass("hover_class");
+	})
+	$("a").mouseleave(function(){
+		console.log("leave");
+		$(this).removeClass("hover_class");
+	})
 });
