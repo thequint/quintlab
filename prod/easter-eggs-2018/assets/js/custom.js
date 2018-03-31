263c3a02747fe84eefaa3202fa6fff0741c47e1a
lab@thequint.com <shahadat.hussain@thequint.com>
2018-03-31 17:03:47 +0530

    Changes

--- prod/easter-eggs-2018/assets/js/custom.js
+++ prod/easter-eggs-2018/assets/js/custom.js
@@ -12,20 +12,37 @@ $(document).ready(function(){
 	$(".egg-top").on("click",function(){
       adjust_iframe_height();
    });
+	if ($(window).width() <= 375) {
+        function handler1() {
+			$(".egg-top .egg-half-full .egg-up").animate({top: "-300px", opacity: "0"});
+			$(".egg-top .egg-half-full .egg-poster").animate({top: "30px", height: "230px"});
+			$(".egg-crack").addClass("fixed");
+			$(".egg-content").fadeIn();
+			$(this).one("click", handler2);
+		}
+		function handler2() {
+			$(".egg-top .egg-half-full .egg-up").animate({top: "1px", opacity: "1"});
+			$(".egg-top .egg-half-full .egg-poster").animate({top: "230px", height: "0"});
+			$(".egg-crack").removeClass("fixed");
+			$(".egg-content").fadeOut();
+			$(this).one("click", handler1);
+		}
+    }else {
+    	function handler1() {
+			$(".egg-top .egg-half-full .egg-up").animate({top: "-300px", opacity: "0"});
+			$(".egg-top .egg-half-full .egg-poster").animate({top: "30px", height: "312px"});
+			$(".egg-crack").addClass("fixed");
+			$(".egg-content").fadeIn();
+			$(this).one("click", handler2);
+		}
+		function handler2() {
+			$(".egg-top .egg-half-full .egg-up").animate({top: "1px", opacity: "1"});
+			$(".egg-top .egg-half-full .egg-poster").animate({top: "312px", height: "0"});
+			$(".egg-crack").removeClass("fixed");
+			$(".egg-content").fadeOut();
+			$(this).one("click", handler1);
+		}
+    }
 	
-	function handler1() {
-		$(".egg-top .egg-half-full .egg-up").animate({top: "-300px", opacity: "0"});
-		$(".egg-top .egg-half-full .egg-poster").animate({top: "30px", height: "312px"});
-		$(".egg-crack").addClass("fixed");
-		$(".egg-content").fadeIn();
-		$(this).one("click", handler2);
-	}
-	function handler2() {
-		$(".egg-top .egg-half-full .egg-up").animate({top: "1px", opacity: "1"});
-		$(".egg-top .egg-half-full .egg-poster").animate({top: "312px", height: "0"});
-		$(".egg-crack").removeClass("fixed");
-		$(".egg-content").fadeOut();
-		$(this).one("click", handler1);
-	}
 	$(".egg-top").one("click", handler1);
 });
