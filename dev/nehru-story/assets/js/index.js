da63599755be97e558ce8b3dfc6d800388247873
https://github.com/shahadatthequint <shahadat.hussain@thequint.com>
2018-05-25 14:45:23 +0530

    New changes

--- dev/nehru-story/assets/js/index.js
+++ dev/nehru-story/assets/js/index.js
@@ -1,96 +1,111 @@
 var audio = document.getElementById("audio-player");
 
 $(document).ready(function() {
-  $("#play-button").click(function() {
-    if ($(this).hasClass("unchecked")) {
-      $(this)
-        .addClass("play-active")
-        .removeClass("play-inactive")
-        .removeClass("unchecked");
-      $(".info-two")
-        .addClass("info-active");
-      $("#pause-button")
-        .addClass("scale-animation-active");
-      $(".waves-animation-one, #pause-button, .seek-field, .volume-icon, .volume-field, .info-two").show();
-      $(".waves-animation-two").hide();
-      $("#pause-button")
-        .children('.icon')
-        .addClass("icon-pause")
-        .removeClass("icon-play");
-      setTimeout(function() {
-        $(".info-one").hide();
-      }, 400);
-      audio.play();
-      audio.currentTime = 0;
-    } else {
-      $(this)
-        .removeClass("play-active")
-        .addClass("play-inactive")
-        .addClass("unchecked");
-      $("#pause-button")
-        .children(".icon")
-        .addClass("icon-pause")
-        .removeClass("icon-play");
-      $(".info-two")
-        .removeClass("info-active");
-      $(".waves-animation-one, #pause-button, .seek-field, .volume-icon, .volume-field, .info-two").hide();
-      $(".waves-animation-two").show();
-      setTimeout(function() {
-        $(".info-one").show();
-      }, 150);
-      audio.pause();
-      audio.currentTime = 0;
-    }
-  });
-  $("#pause-button").click(function() {
-    $(this).children(".icon")
-      .toggleClass("icon-pause")
-      .toggleClass("icon-play");
+    $("#play-button").click(function() {
+        if ($(this).hasClass("unchecked")) {
+            $(this)
+                .addClass("play-active")
+                .removeClass("play-inactive")
+                .removeClass("unchecked");
+            $(".info-two")
+                .addClass("info-active");
+            $("#pause-button")
+                .addClass("scale-animation-active");
+            $(".waves-animation-one, #pause-button, .seek-field, .volume-icon, .volume-field, .info-two").show();
+            $(".waves-animation-two").hide();
+            $("#pause-button")
+                .children('.icon')
+                .addClass("icon-pause")
+                .removeClass("icon-play");
+            setTimeout(function() {
+                $(".info-one").hide();
+            }, 400);
+            audio.play();
+            audio.currentTime = 0;
+            
+            $(".container--left").addClass("img-animate");
+            $(".container--right").addClass("box-width");
+            $(".container--right").addClass("add-content");
+            if (type_flag == 0) {
+                typewriter();
+                type_flag = 1;
+            }
+            // call
+           // playBTNFunction($(this));
+        } else {
+            $(this)
+                .removeClass("play-active")
+                .addClass("play-inactive")
+                .addClass("unchecked");
+            $("#pause-button")
+                .children(".icon")
+                .addClass("icon-pause")
+                .removeClass("icon-play");
+            $(".info-two")
+                .removeClass("info-active");
+            $(".waves-animation-one, #pause-button, .seek-field, .volume-icon, .volume-field, .info-two").hide();
+            $(".waves-animation-two").show();
+            setTimeout(function() {
+                $(".info-one").show();
+            }, 150);
+            audio.pause();
+            audio.currentTime = 0;
 
-    if (audio.paused) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  });
-  $("#play-button").click(function() {
-    setTimeout(function() {
-      $("#play-button").children(".icon")
-        .toggleClass("icon-play")
-        .toggleClass("icon-cancel");
-    }, 350);
-  });
-  $(".like").click(function() {
-    $(".icon-heart").toggleClass("like-active");
-  });
+             $(".container--left").removeClass("img-animate");
+            $(".container--right").removeClass("box-width");
+            $(".container--right").removeClass("add-content");
+        }
+        adjust_iframe_height();
+    });
+    $("#pause-button").click(function() {
+        $(this).children(".icon")
+            .toggleClass("icon-pause")
+            .toggleClass("icon-play");
+
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+    $("#play-button").click(function() {
+        setTimeout(function() {
+            $("#play-button").children(".icon")
+                .toggleClass("icon-play")
+                .toggleClass("icon-cancel");
+        }, 350);
+    });
+    $(".like").click(function() {
+        $(".icon-heart").toggleClass("like-active");
+    });
 });
 
 function CreateSeekBar() {
-  var seekbar = document.getElementById("audioSeekBar");
-  seekbar.min = 0;
-  seekbar.max = audio.duration;
-  seekbar.value = 0;
+    var seekbar = document.getElementById("audioSeekBar");
+    seekbar.min = 0;
+    seekbar.max = audio.duration;
+    seekbar.value = 0;
 }
 
 function EndofAudio() {
-  document.getElementById("audioSeekBar").value = 0;
+    document.getElementById("audioSeekBar").value = 0;
 }
 
 function audioSeekBar() {
-  var seekbar = document.getElementById("audioSeekBar");
-  audio.currentTime = seekbar.value;
+    var seekbar = document.getElementById("audioSeekBar");
+    audio.currentTime = seekbar.value;
 }
 
 function SeekBar() {
-  var seekbar = document.getElementById("audioSeekBar");
-  seekbar.value = audio.currentTime;
+    var seekbar = document.getElementById("audioSeekBar");
+    seekbar.value = audio.currentTime;
 }
 
 audio.addEventListener("timeupdate", function() {
-  var duration = document.getElementById("duration");
-  var s = parseInt(audio.currentTime % 60);
-  var m = parseInt((audio.currentTime / 60) % 60);
-  duration.innerHTML = m + ':' + s;
+    var duration = document.getElementById("duration");
+    var s = parseInt(audio.currentTime % 60);
+    var m = parseInt((audio.currentTime / 60) % 60);
+    duration.innerHTML = m + ':' + s;
 }, false);
 
 Waves.init();
