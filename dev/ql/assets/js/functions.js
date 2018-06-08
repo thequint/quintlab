1ac2d0202b6fb8c03cd9138c0f57349517c2ab23
Achintya De <achintya.de@thequint.com>
2018-06-08 12:35:54 +0530

    update

--- dev/ql/assets/js/functions.js
+++ dev/ql/assets/js/functions.js
@@ -1,3 +1,29 @@
+
+$("#scrollDown").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+     });
+
+
+
+$(".share-ico").click(function (e) {
+	$(".social-ico").slideToggle(400);
+	return false;
+});
+
+$("body").click(function () {
+	$(".social-ico").slideUp();
+});
+
+
 new WOW().init();
 
 var collectionSlug = 'the-quint-lab';
