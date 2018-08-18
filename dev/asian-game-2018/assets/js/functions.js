97421249e438bbfaa648558f34bfb2ce9458fde5
Quint LAB <shahadat.hussain@thequint.com>
2018-08-18 16:16:32 +0530

    Slider Changes

--- dev/asian-game-2018/assets/js/functions.js
+++ dev/asian-game-2018/assets/js/functions.js
@@ -20,21 +20,33 @@ function showInfo(data) {
     // sheet data
 
     for (var index = data.length - 1; index >= 0; index--) {
-        
-            $("#product_show").append(
-                "<li class='asian-game__content " + data[index].Game_icon + "'>" +
-                    "<div class='el-position'>" +
-                        "<div class='asian-game__content--timestamp'>" + data[index].Game_time + "</div>" +
-                        "<div class='asian-game__content--category'><span class='cat-icon'></span><span class='cat-bg'>" + data[index].Game_category + "</span></div>" +
-                        "<div class='asian-game__content--headline'>" + data[index].Game_player_name + " <span class='match-vs'>" + data[index].Game_player_vs + "</span></div>" +
-                        "<div class='asian-game__content--sub-headline'>" + data[index].Game_round + "</div>" +
-                    "</div>" +
-                    "<span class='circle-small'></span>" +
-                    //"<span class='circle-large'><span></span></span>" +
-                "</li>"
-            )
-
-    }
+       if(data[index] && data[index].Game_player_vs){
+           $("#product_show").append(
+               "<li class='asian-game__content " + data[index].Game_icon + "'>" +
+                   "<div class='el-position'>" +
+                       "<div class='asian-game__content--timestamp'>" + data[index].Game_time + "</div>" +
+                       "<div class='asian-game__content--category'><span class='cat-icon'></span><span class='cat-bg'>" + data[index].Game_category + "</span></div>" +
+                       "<div class='asian-game__content--headline'>" + data[index].Game_player_name + " <span class='match-vs'>" + data[index].Game_player_vs + "</span></div>" +
+                       "<div class='asian-game__content--sub-headline'>" + data[index].Game_round + "</div>" +
+                   "</div>" +
+                   "<span class='circle-large'><span></span></span>" +
+               "</li>"
+           )
+       } else {
+           $("#product_show").append(
+               "<li class='asian-game__content " + data[index].Game_icon + "'>" +
+                   "<div class='el-position'>" +
+                       "<div class='asian-game__content--timestamp'>" + data[index].Game_time + "</div>" +
+                       "<div class='asian-game__content--category'><span class='cat-icon'></span><span class='cat-bg'>" + data[index].Game_category + "</span></div>" +
+                       "<div class='asian-game__content--headline'>" + data[index].Game_player_name + "</div>" +
+                       "<div class='asian-game__content--sub-headline'>" + data[index].Game_round + "</div>" +
+                   "</div>" +
+                   "<span class='circle-small'></span>" +
+               "</li>"
+           )
+       }
+   }
+   $("#schedule_slider").show();
 
 adjust_slider();
 }
