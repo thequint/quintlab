b2cb3010aad3f8b1e68e44e260220a99539ff12d
Quint LAB <shahadat.rockstar@gmail.com>
2018-10-27 18:52:55 +0530

    Pushing New Project Air Quality

---
+++ dev/air-quality/assets/js/data.js
@@ -0,0 +1,163 @@
+//autocomplete
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                //b.attr('data-index',"i");
+                /*make the matching letters bold:*/ //console.log(i);
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    // alert(b.getAttribute('data-index'));
+                    append_data(this.getAttribute('data-index'))
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+                b.setAttribute('data-index', arr[i].substr(arr[i].indexOf(' ') + 1));
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function(e) {
+
+        closeAllLists(e.target);
+    });
+}
+
+// API
+
+var items = []
+$.getJSON("https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd0000013a689be75bc04fee4825558307eb7390&format=json&offset=0&limit=10000", function(data) {
+    //console.log(data);
+    items = data.records;
+    var city_arr = [];
+    for (var i = 0; i < data.records.length; i++) {
+        if (data.records[i].pollutant_id == "PM2.5") city_arr.push(items[i].city + " " + items[i].station);
+    }
+    //console.log(city_arr);
+ 
+    append_data ("Anand Vihar, Delhi - DPCC");
+    autocomplete(document.getElementById("myInput"), city_arr);
+
+    //console.log(items[100].city+", "+items[100].station);
+});
+
+// Add data to the table on select
+
+
+
+function append_data(station) {
+
+    $(".table_data").html('<tr><th>Pollutant</th><th>Avg</th><th>Min</th><th>Max</th></tr>');
+
+    console.log(station);
+
+    // console.log(items[100]);
+
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].station == station) {
+            // console.log(items[i]);
+
+            // console.log(i + "|" + items[i].station + "|" + items[i].id);
+
+            // $(".table_data").append("<li> Pollutant = " + items[i].pollutant_id + "| Avg value =" + items[i].pollutant_avg + "| Min value =" + items[i].pollutant_min + "| Max value =" + items[i].pollutant_max + "</li>");
+
+            $(".table_data").append("<tr><td>" + items[i].pollutant_id + "</td><td>" + items[i].pollutant_avg + "</td><td>" + items[i].pollutant_min + "</td><td>" + items[i].pollutant_max + "</td></tr>");
+
+            if (items[i].pollutant_id == "PM2.5") {
+                // call pie chart function
+                console.log("check");
+                $(".quality-index").text(items[i].pollutant_avg);
+                console.log(items[i].pollutant_avg/500*100*(180/100));
+
+                $(".quality-load").css("transform","rotate("+items[i].pollutant_avg/500*100*(180/100)+"deg)")
+            }
+
+        }
+    }
+    $(".data-station").html(station);
+
+
+}
