a85a8fb5845d04c7e0abe8492fd06375751fef85
Quint LAB <shahadat.rockstar@gmail.com>
2019-01-05 16:35:58 +0530

    Updated Ad Microsite

--- dev/ad-microsite/assets/js/functions.js
+++ dev/ad-microsite/assets/js/functions.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
-
-    $(".media-play-icon").on("click", function() {
-        $(".modal-popup").show();
-    });
-
-    $(".popup-close").on("click", function() {
-        $(".modal-popup").hide();
-    });
-
-    $(".tab-nav span").on("click", function(){
-        $(".tab-nav span").removeClass("active");
-        $(this).addClass("active");
-
-        $(".tab-content").fadeOut();
-        $(".tab-content").eq($(this).index()).fadeIn();
-    });
-
-    jQuery(document).ready(function($) {
-        $('.smobitrigger').smplmnu();
-    });
-
     $('.slider-first-ad').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -85,6 +64,95 @@ $(document).ready(function() {
         prevArrow: $('.prev-third'),
         nextArrow: $('.next-third')
     });
+
+
+
+    $(".icon-desktop").on("click", function() {
+        $(".modal-ads-desktop").show();
+    });
+
+    $(".desktop-close").on("click", function() {
+        $(".modal-ads-desktop").hide();
+        $(".desktop-box__screen").html('');
+    });
+
+    $(".interstitial-btn-wrap .icon-desktop").on("click", function() {
+        console.log("List:"+$(this).closest("li").index());
+
+        if($(this).closest("li").index() == 0){
+            $(".desktop-box__screen").html('<iframe src="https://www.thequint.com/quintlab/widgets/internal-ad/vibrating-mobile-ad/vib_r.html" width="100%" frameborder="0"></iframe>');
+
+        } if($(this).closest("li").index() == 1){
+            $(".desktop-box__screen").html('<iframe src="https://www.thequint.com/quintlab/widgets/internal-ad/page_peel/page_peel.html" width="100%" frameborder="0"></iframe>');
+
+        } if($(this).closest("li").index() == 2){
+            $(".desktop-box__screen").html('<iframe class="interstitial-iframe-mowgli-360" src="https://www.thequint.com/quintlab/widgets/internal-ad/mowgli-360-video-interstitial/index.html" width="100%" frameborder="0"></iframe>');
+
+        } if($(this).closest("li").index() == 3){
+            $(".desktop-box__screen").html('<iframe class="interstitial-iframe-tilt" src="https://www.thequint.com/quintlab/widgets/internal-ad/final_tilt/index.html" width="100%" frameborder="0"></iframe>');
+        }
+
+        // } if($(this).closest("li").index() == 2){
+        //     $(".desktop-box__screen").html('<iframe class="iframe-adidas" src="https://www.thequint.com/quintlab/widgets/internal-ad/adidas_chat/adiads.html" width="100%" frameborder="0" ></iframe>');
+
+        // } if($(this).closest("li").index() == 3){
+        //     $(".desktop-box__screen").html('<iframe class="iframe-mowgli-banner" src="https://www.thequint.com/quintlab/widgets/internal-ad/mowgli_top_banner_ad/mowgli_banner_ad.html" width="100%" frameborder="0" ></iframe>');
+        // }
+    });
+
+
+    $(".icon-mobile").on("click", function() {
+        $(".modal-ads-mobile").show();
+    });
+
+    $(".mobile-close").on("click", function() {
+        $(".modal-ads-mobile").hide();
+        $(".mobile-box__screen").html('');
+    });
+
+    $(".interstitial-btn-wrap .icon-mobile").on("click", function() {
+        console.log("List:"+$(this).closest("li").index());
+
+        if($(this).closest("li").index() == 0){
+            $(".mobile-box__screen").html('<iframe src="https://www.thequint.com/quintlab/widgets/internal-ad/vibrating-mobile-ad/vib_r.html" width="100%" frameborder="0"></iframe>');
+
+        }if ($(this).closest("li").index() == 1){
+            $(".mobile-box__screen").html('<iframe src="https://www.thequint.com/quintlab/widgets/internal-ad/page_peel/page_peel.html" width="100%" frameborder="0"></iframe>');
+
+        }if ($(this).closest("li").index() == 2){
+            $(".mobile-box__screen").html('<iframe class="interstitial-iframe-mowgli-360" src="https://www.thequint.com/quintlab/widgets/internal-ad/mowgli-360-video-interstitial/index.html" width="100%" frameborder="0"></iframe>');
+
+        }if ($(this).closest("li").index() == 3){
+            $(".mobile-box__screen").html('<iframe class="interstitial-iframe-tilt" src="https://www.thequint.com/quintlab/widgets/internal-ad/final_tilt/index.html" width="100%" frameborder="0"></iframe>');
+        }
+    });
+
+
+
+
+    $(".media-play-icon").on("click", function() {
+        $(".modal-popup").show();
+    });
+
+    $(".popup-close").on("click", function() {
+        $(".modal-popup").hide();
+    });
+
+    $(".tab-nav span").on("click", function(){
+        $(".tab-nav span").removeClass("active");
+        $(this).addClass("active");
+
+        $(".tab-content").fadeOut();
+        $(".tab-content").eq($(this).index()).fadeIn();
+    });
+
+    jQuery(document).ready(function($) {
+        $('.smobitrigger').smplmnu();
+    });
+
+    
+
+
 
 
     // Interstitial ad
