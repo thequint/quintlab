42eace2d1bfeb4db089fe473f0cd1f882d4bbd99
shahadat321 <shahadat.rockstar@gmail.com>
2019-04-23 14:44:32 +0530

    New UI of Airtel IPL

---
+++ dev/airtel-ipl/assets/js/functions.js
@@ -0,0 +1,27 @@
+$(".popup-bt").click(function(e) {
+    $(".overlay").fadeIn();
+    $(".info-popup").fadeIn();
+    $(".info-popup h2").html($(this).attr("data-headline"));
+    $(".info-popup p").html($(this).attr("data-text"));
+
+    $(".overlay").addClass($(this).attr("data-class"));
+});
+
+$(".close-icon").click(function() {
+    $(".overlay").hide();
+    $(".info-popup").hide();
+    $(".overlay").attr("class","overlay");
+});
+
+// Document Click
+$(document).mouseup(function (e){
+    var container = $(".info-popup");
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0)
+    {
+        container.hide();
+        $(".overlay").hide();
+        $(".overlay").attr("class","overlay");
+    }
+    // $(".content-box li").removeClass("active");
+});
